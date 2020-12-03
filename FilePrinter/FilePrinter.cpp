e397c97cb83808f84973360b03f27e1b58950325
david-soto-m <davidsotomarchena@protonmail.com>
2020-12-04 00:57:46 +0100

    EOD commit

--- FilePrinter/FilePrinter.cpp
+++ FilePrinter/FilePrinter.cpp
@@ -9,11 +9,14 @@ Printer::~Printer(){
 	file.close();
 }
 
+void translator(){
+	//TODO This function should be ready to change from function evaluated pairs of values to the character number where you should print the token
+}
 
 void Printer::print_base(){
-	
+	//TODO this should paint the axes on the file and generally prepare the file
 }
 
 void Printer::print_function(double **list){
-	//TODO in the end this wil recieve a function object which will be passed the axes object
+	//TODO in the future this wil recieve a function object which will be passed the axes object
 }
--- FilePrinter/FilePrinter.h
+++ FilePrinter/FilePrinter.h
@@ -6,15 +6,16 @@ using namespace std;
 
 class Printer{
 	typedef struct {
-		int x_min=0,x_max=10,y_min=0,y_max=10;
+		int x_min=0,x_max=10,x_step=1,y_min=0,y_max=10,y_step=1;
 	} axes;
 	private:
 		fstream file;
 		int width, height;
+		int *buffer;
 		axes ax;
 		void translator(double **list_orig,double **list,int width);
 	public:
-		Printer(char *file,int width,int height);
+		Printer(char *file,int width=11,int height=11);
 		~Printer();
 		void print_base();
 		void print_function(double **list);
