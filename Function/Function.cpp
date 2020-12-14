ffb1ea4f1c7ad1e7d3e584889b8d87818e532be8
david-soto-m <davidsotomarchena@protonmail.com>
2020-12-14 01:03:56 +0100

    Parser

--- Function/Function.cpp
+++ Function/Function.cpp
@@ -1,19 +1,19 @@
 #include "Function.h"
 #include <cmath>
 void Function::eval(Axis ax,Axis ax_theta){
-	space_x(ax);
-	double x;
-	x=ax.get_x_min();
-	for(int i=0;i<length;i++){
-		pointlist[i][1]=-(sin(x)+pow(x,2));
-		x+=ax.get_x_step();
-	}
-// 	space_x(ax_theta);
-// 	double theta;
-// 	theta=ax_theta.get_x_min();
+// 	space_x(ax);
+// 	double x;
+// 	x=ax.get_x_min();
 // 	for(int i=0;i<length;i++){
-// 		pointlist[i][1]=theta;
-// 		theta+=ax_theta.get_x_step();
+// 		pointlist[i][1]=1/(x);
+// 		x+=ax.get_x_step();
 // 	}
-// 	polar_to_cartesian();
+	space_x(ax_theta);
+	double theta;
+	theta=ax_theta.get_x_min();
+	for(int i=0;i<length;i++){
+		pointlist[i][1]=2*(1-cos(theta));
+		theta+=ax_theta.get_x_step();
+	}
+	polar_to_cartesian();
 }
