d1c99e73957e1743390f2ed2746551c1706333cc
david-soto-m <davidsotomarchena@protonmail.com>
2022-07-20 12:13:10 +0200

    update

--- Function/Function.cpp
+++ Function/Function.cpp
@@ -9,12 +9,12 @@ void Function::eval(Axis ax,Axis ax_theta){
 // 		pointlist[i][1]=1/(x);
 // 	}
 
-	space_y(ax);
-	double y;
-	for(int i=0;i<length;i++){
-		y=pointlist[i][1];
-		pointlist[i][0]=pow(y,2);
-	}
+// 	space_y(ax);
+// 	double y;
+// 	for(int i=0;i<length;i++){
+// 		y=pointlist[i][1];
+// 		pointlist[i][0]=pow(y,2);
+// 	}
 	
 // 	space_x(ax_theta);
 // 	double theta;
@@ -24,18 +24,18 @@ void Function::eval(Axis ax,Axis ax_theta){
 // 	}
 // 	polar_to_cartesian();
 	
-// 	space_xy(ax);
-// 	double x=ax.get_x_min(),y;
-// 	for(int i=0;i<ax.width();i++){
-// 		y=ax.get_y_min();
-// 		for(int j=0;j<ax.height();j++){
-// 			if (pow(x,2)+pow(y,2)>24&&pow(x,2)+pow(y,2)<26){
-// 				pointlist[i*ax.width()+j][0]=x;
-// 				pointlist[i*ax.width()+j][1]=y;
-// 			}
-// 			y+=ax.get_y_step();
-// 		}
-// 		std::cout<<x<<","<<y<<"\n";
-// 		x+=ax.get_x_step();
-// 	}
+	space_xy(ax);
+	double x=ax.get_x_min(),y;
+	for(int i=0;i<ax.width();i++){
+		y=ax.get_y_min();
+		for(int j=0;j<ax.height();j++){
+			if (pow(x,2)+pow(y,2)>24&&pow(x,2)+pow(y,2)<26){
+				pointlist[i*ax.width()+j][0]=x;
+				pointlist[i*ax.width()+j][1]=y;
+			}
+			y+=ax.get_y_step();
+		}
+		std::cout<<x<<","<<y<<"\n";
+		x+=ax.get_x_step();
+	}
 }
--- compilescript.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++  -g main.cpp FilePrinter/FilePrinter.cpp Axis/Axis.cpp Function/PureFunction.cpp Function/Function.cpp Parser/Parser.cpp -o main
